6684a69f9edfcc1053ddac0d9b619e495eb75cd6
Ivan Nemtsev <ixtreme3@yandex.ru>
2022-04-23 21:15:06 +0700

    Задание по асинхронности

--- src/Async.ts
+++ src/Async.ts
@@ -3,27 +3,17 @@
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+  return new Promise(resolve => {
+    setTimeout(() => {}, ms);
+    resolve(ms);
+  });
 }
 
 /*
 Перепишите функцию getData так, чтобы она выполнялась быстрее.
  */
 export function getData(): Promise<number[]> {
-    const result: number[] = [];
-
-    return mock(100)
-        .then((data1) => {
-            result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
-            result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
-            result.push(data3);
-            return result;
-        });
+    return Promise.all([mock(100), mock(200), mock(300)]);
 }
 
 /*
@@ -32,8 +22,10 @@ export function getData(): Promise<number[]> {
  */
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
+        throw Promise.reject(new Error('my error'));
     } catch (err) {
-        return err.message;
+        if (err instanceof Promise) {
+          return err.catch(error => error.message);
+        } else return new Promise(resolve => resolve(undefined))
     }
 }
